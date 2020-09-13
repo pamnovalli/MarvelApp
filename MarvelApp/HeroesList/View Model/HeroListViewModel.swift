106ae4bdb21725533f5094d9344eb6f1cfe84874
Pamela Ianovalli <pamela.ianovalli@ifood.com.br>
2020-09-13 17:24:09 -0300

    Updated to make the view controller passive

--- MarvelApp/HeroesList/View Model/HeroListViewModel.swift
+++ MarvelApp/HeroesList/View Model/HeroListViewModel.swift
@@ -8,35 +8,79 @@
 
 import UIKit
 
-protocol HeroesListViewModelDelegate {
-    func didLoadHeroes()
+protocol HeroesListViewModelDelegate: AnyObject {
+    func setHeroes(heroes: [Hero])
+    func setTitle(_ title: String)
+    func tableViewReloadData()
+    func setupSearchBar(title: String)
 }
 
-class HeroesListViewModel {
-    var currentPage = 0
-    var heroes: [Hero] = []
-    var total = 0
-    var loadingHeroes = false
-    let service: HeroesListService
-    var delegate: HeroesListViewModelDelegate?
-    var filteredHero: [Hero] = []
+final class HeroesListViewModel {
+    private let service: HeroesListService
+    private weak var delegate: HeroesListViewModelDelegate?
+    private var currentPage = 0
+    private var total = 0
+    private var heroes: [Hero] = []
+    private var filteredHeroes: [Hero] = []
+    private var loadingHeroes = false
     
     init(service: HeroesListService = HeroesListService()) {
         self.service = service
     }
-
     
-    func loadHeroes(searchHero: String?) {
-        loadingHeroes = true
-        service.page = currentPage
-        service.callService(heroName: searchHero) { (heroes) in
-            if let heroes = heroes {
-                self.heroes += heroes.data.results
-                self.total = heroes.data.total
-                self.delegate?.didLoadHeroes()
-            }
-        }
+    func viewDidLoad(delegate: HeroesListViewModelDelegate) {
+        self.delegate = delegate
         
+        loadHeroes()
+        delegate.setupSearchBar(title: "Search hero")
+        delegate.setTitle("Marvel Heroes")
     }
     
+    func willDisplay(indexPath: Int) {
+        if indexPath == heroes.count - 10 && heroes.count != total && loadingHeroes == false {
+            currentPage += 1
+            
+            loadHeroes()
+        }
+    }
+    
+    func searchBarSearchButtonClicked(inputText: String?) {
+        guard let text = inputText, text.count >= 3 else {
+            return
+        }
+        
+        searchHeroes(heroName: text)
+    }
+    
+    func searchBarCancelButtonClicked() {
+        delegate?.setHeroes(heroes: heroes)
+        delegate?.tableViewReloadData()
+    }
+    
+    private func loadHeroes() {
+        loadingHeroes.toggle()
+        
+        service.loadHeroes(currentPage: currentPage) { [weak self] heroInfo in
+            let heroesData = heroInfo.data
+            
+            self?.total = heroesData.total
+            self?.heroes += heroesData.results
+            self?.delegate?.setHeroes(heroes: self?.heroes ?? [])
+            self?.loadingHeroes.toggle()
+            self?.delegate?.tableViewReloadData()
+        }
+    }
+    
+    private func searchHeroes(heroName: String) {
+        loadingHeroes.toggle()
+
+        service.searchHeroes(heroName: heroName) { [weak self] heroInfo in
+            let heroesData = heroInfo.data
+            
+            self?.filteredHeroes = heroesData.results
+            self?.delegate?.setHeroes(heroes: self?.filteredHeroes ?? [])
+            self?.loadingHeroes.toggle()
+            self?.delegate?.tableViewReloadData()
+        }
+    }
 }
