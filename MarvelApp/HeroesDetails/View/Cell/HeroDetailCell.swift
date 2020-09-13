cff3d120b471c9be5df73a6f99db688c82fd9f73
Pamela Ianovalli <pamela.ianovalli@ifood.com.br>
2020-09-13 19:31:00 -0300

    Added reuse cell method

--- MarvelApp/HeroesDetails/View/Cell/HeroDetailCell.swift
+++ MarvelApp/HeroesDetails/View/Cell/HeroDetailCell.swift
@@ -9,16 +9,13 @@
 import UIKit
 
 class HeroDetailCell: UITableViewCell {
-    
-    @IBOutlet weak var lblItemName: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
+    @IBOutlet private weak var lblItemName: UILabel!
     
     func prepareCell(with item: ComicsItem) {
         lblItemName.text = item.name
     }
     
+    func reuse() {
+        lblItemName.text = nil
+    }
 }
-
