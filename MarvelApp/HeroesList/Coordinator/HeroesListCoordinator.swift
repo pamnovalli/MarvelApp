614cbef3420c2ba341e99a49fb49daadf8b23bf4
Pamela Ianovalli <pamela.ianovalli@ifood.com.br>
2020-09-13 17:22:57 -0300

    Updated to conform Coordinator protocol

--- MarvelApp/HeroesList/Coordinator/HeroesListCoordinator.swift
+++ MarvelApp/HeroesList/Coordinator/HeroesListCoordinator.swift
@@ -6,26 +6,25 @@
 //  Copyright © 2020 Pamela Ianovalli. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-
-protocol HeroesListCoordinatorProtocol {
-    func navigateToHeroesDetail(selectedHeroId: Int)
+final class HeroesListCoordinator: Coordinator {
+    private let navigator: UINavigationController
+    
+    init(navigator: UINavigationController) {
+        self.navigator = navigator
+    }
+    
+    func start() {
+        let viewController = MarvelHeroesViewController()
+        viewController.delegate = self
+        navigator.pushViewController(viewController, animated: true)
+    }
 }
 
-class HeroesListCoordinator: HeroesListCoordinatorProtocol {
-
-    
-    weak var viewController: MarvelHeroesViewController?
-    
-    init(viewController: MarvelHeroesViewController) {
-        self.viewController = viewController
+extension HeroesListCoordinator: MarvelHeroesViewControllerDelegate {
+    func didSelectRowAt(selectedHeroId: Int) {
+        let nextCoordinator = HeroDetailCoordinator(navigator: navigator, selectedHeroId: selectedHeroId)
+        nextCoordinator.start()
     }
-    
-    func navigateToHeroesDetail(selectedHeroId: Int) {
-        let nextViewController = MarvelHeroesDetailViewController(nibName: "MarvelHeroesDetailViewController", bundle: Bundle.main)
-        nextViewController.viewModel.heroId = selectedHeroId
-        viewController?.navigationController?.pushViewController(nextViewController, animated: true)
-    }
-    
 }
